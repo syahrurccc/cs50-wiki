4d0bff03138281d28fcdc748241bdc0322f596cd
syahrurccc <syahrur.ramadhan94@gmail.com>
2025-07-30 14:55:46 +0800

    added entry support

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from markdown2 import Markdown
+from pathlib import Path
 
 from . import util
 
+markdowner = Markdown()
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    for entry in util.list_entries():
+        if title.lower() == entry.lower():
+            p = Path(f"entries/{entry}.md")
+            page = p.read_text()
+            return render(request, "encyclopedia/entry.html", {
+                "entry": markdowner.convert(page),
+                "title": entry
+            })
+
+    return HttpResponse("404 Page not Found")
